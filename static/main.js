a74fe6f9d5a919eaa3768f4ae983e34b076f0cd1
Ubuntu <ubuntu@ip-172-31-47-157.ec2.internal>
2019-08-23 17:15:24 +0000

    first commits/riddle game

--- static/main.js
+++ static/main.js
@@ -12,9 +12,37 @@ function save_name(){
 }
 
 
+var inputs = $("form#contactForm input, form#contactForm textarea");
+
+var validateInputs = function validateInputs(inputs) {
+  var validForm = true;
+  inputs.each(function(index) {
+    var input = $(this);
+    if (!input.val() || (input.type === "grade" && !input.is(':checked'))) {
+      $("#sendMessageButton1").attr("disabled", "disabled");
+      validForm = false;
+    }
+  });
+  return validForm;
+}
 
 
-
+inputs.each(function() {
+  var input = $(this);
+  if (input.type === "grade") {
+    input.change(function() {
+      if (validateInputs(inputs)) {
+        $("#sendMessageButton1").removeAttr("disabled");
+      }
+    });
+  } else {
+    input.keyup(function() {
+      if (validateInputs(inputs)) {
+        $("#sendMessageButton1").removeAttr("disabled");
+      }
+    });
+  }
+});
 
 
 
